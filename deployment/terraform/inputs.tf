f82f129c28f7191f5094232888ac684e01476a81
Karl Cardenas <karl@spectrocloud.com>
2023-12-13 17:02:21 -0700

    chore: updated deployment version

--- deployment/terraform/inputs.tf
+++ deployment/terraform/inputs.tf
@@ -34,7 +34,7 @@ variable "mendable_api_key" {
 variable "image" {
   type        = string
   description = "The Spectromate image to deploy."
-  default     = "ghcr.io/spectrocloud/spectromate:v1.0.5"
+  default     = "ghcr.io/spectrocloud/spectromate:v1.0.6"
 }
 
 variable "cluster_name" {
